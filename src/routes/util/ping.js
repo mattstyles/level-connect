2f803613a76bd1e3cea41fcb983aa177affac84f
mattstyles <matt@veryfizzyjelly.com>
2015-09-13 22:31:44 +0100

    #update ping route logging

--- src/routes/util/ping.js
+++ src/routes/util/ping.js
@@ -1,10 +1,5 @@
 
-import CONSTANTS from '../../constants'
-import log from '../../log'
-
 export default function *( next ) {
-    log.info( this.request.method, this.request.url, 'OK', this.request.ip )
-
     this.onSuccess({
         status: 200,
         body: {
