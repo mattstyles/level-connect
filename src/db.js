d44c7a9b8a94b503803a3b6c859ac491073f9c07
Matt Styles <matt@veryfizzyjelly.com>
2016-03-16 15:02:47 +0000

    #add ref to created sublevels

--- src/db.js
+++ src/db.js
@@ -5,14 +5,33 @@ import then from 'then-levelup'
 
 import CONSTANTS from './constants'
 
+/**
+ * Wrap root instance
+ */
 const dbpath = CONSTANTS.CONNECT_PATH || CONSTANTS.DEFAULT_PATH
 const level = party( dbpath, CONSTANTS.DB_OPTS )
 const root = sublevel( level )
 
 export default root
 
+
+/**
+ * Expose connected clients db
+ */
 export const clients = then( root.sublevel( '_client' ), CONSTANTS.DB_OPTS )
 
+
+/**
+ * Create a reference to sublevels
+ */
+let sublevels = new Map()
+
 export function getSublevel( sublevel ) {
-  return then( root.sublevel( sublevel, CONSTANTS.DB_OPTS ) )
+  if ( sublevels.has( sublevel ) ) {
+    return sublevels.get( sublevel )
+  }
+
+  let newSublevel = then( root.sublevel( sublevel, CONSTANTS.DB_OPTS ) )
+  sublevels.set( sublevel, newSublevel )
+  return newSublevel
 }
